11ffc7c2f81b5487d3423f4cac9b8d3ad83ef12c
lyndsiWilliams <kcatgirl@gmail.com>
2021-02-24 18:35:57 -0600

    Section 2.4 complete

--- color-button/src/App.test.js
+++ color-button/src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
 test('button has correct initial color', () => {
@@ -9,8 +9,13 @@ test('button has correct initial color', () => {
 
   // Expect the background color to be red
   expect(colorButton).toHaveStyle({ backgroundColor: 'red' });
-});
 
-test('button turns blue when clicked', () => {
-  
+  // Click the button
+  fireEvent.click(colorButton);
+
+  // Expect the background color to be blue
+  expect(colorButton).toHaveStyle({ backgroundColor: 'blue' });
+
+  // Expect the button text to be 'Change to red'
+  expect(colorButton.textContent).toBe('Change to red');
 });
