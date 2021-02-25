6363b72311c037cd6403159ae82c83f2ba0f88d9
lyndsiWilliams <kcatgirl@gmail.com>
2021-02-24 18:25:52 -0600

    Section 2.2 complete

--- color-button/src/App.test.js
+++ color-button/src/App.test.js
@@ -1,8 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('button has correct initial color', () => {
   render(<App />);
-  const linkElement = screen.getByRole('link', { name: /learn react/i });
-  expect(linkElement).toBeInTheDocument();
+
+  // Find an element with a role of button and the text 'Change to blue'
+  const colorButton = screen.getByRole('button', { name: 'Change to blue' });
+
+  // Expect the background color to be red
+  expect(colorButton).toHaveStyle({ backgroundColor: 'red' });
+});
+
+test('button turns blue when clicked', () => {
+  
 });
