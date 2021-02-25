e405a67d718a7758fc28883091c562e0d1c3621e
lyndsiWilliams <kcatgirl@gmail.com>
2021-02-24 18:41:18 -0600

    Section 2.6 complete

--- color-button/src/App.test.js
+++ color-button/src/App.test.js
@@ -19,3 +19,15 @@ test('button has correct initial color', () => {
   // Expect the button text to be 'Change to red'
   expect(colorButton.textContent).toBe('Change to red');
 });
+
+test('initial conditions', () => {
+  render(<App />);
+
+  // check that the button starts out enabled
+  const colorButton = screen.getByRole('button', { name: 'Change to blue' });
+  expect(colorButton).toBeEnabled();
+
+  // check that the checkbox starts out unchecked
+  const checkbox = screen.getByRole('checkbox');
+  expect(checkbox).not.toBeChecked();
+})
